ce1e1ff252e22ad1acc62bca3fd9967a42e6f9c8
Julie Luciani <julie.lcny@gmail.com>
2024-10-31 15:45:05 +0100

    separate api's controller logic

--- backend/app/controller/api_controller.js
+++ backend/app/controller/api_controller.js
@@ -3,7 +3,7 @@ import path from "path";
 import { v4 as uuidv4 } from "uuid";
 
 // Fonction pour gérer l'upload
-export function handleUploadFiles(req, res) {
+export const handleUploadFiles = (req, res) => {
   // Vérifie s'il y a bien un fichier
   if (!req.file) {
     return res
@@ -32,4 +32,26 @@ export function handleUploadFiles(req, res) {
       fileName: newFileName,
     });
   });
-}
+};
+
+export const getUploadsSize = (req, res) => {
+  const uploadDir = path.join(process.cwd(), "uploads");
+
+  fs.readdir(uploadDir, (err, files) => {
+    if (err) {
+      console.error("Erreur lors de la lecture du dossier :", err);
+      return res
+        .status(500)
+        .json({ error: "Erreur lors de la lecture du dossier" });
+    }
+
+    let totalSize = 0;
+    files.forEach((file) => {
+      const filePath = path.join(uploadDir, file);
+      totalSize += fs.statSync(filePath).size;
+    });
+
+    const sizeInGB = totalSize / (1024 * 1024 * 1024);
+    res.json({ sizeInGB });
+  });
+};
--- backend/app/route/api.js
+++ backend/app/route/api.js
@@ -2,7 +2,10 @@ import express from "express";
 import multer from "multer";
 import fs from "fs";
 import path from "path";
-import { handleUploadFiles } from "../controller/api_controller.js";
+import {
+  handleUploadFiles,
+  getUploadsSize,
+} from "../controller/api_controller.js";
 
 const router = express.Router();
 const upload = multer({ dest: path.join(process.cwd(), "uploads") }); // Assurez-vous que multer utilise le bon chemin
@@ -13,27 +16,6 @@ router.post("/upload", upload.single("file"), (req, res) => {
 });
 
 // Route pour vérifier la taille du dossier uploads
-router.get("/size", (req, res) => {
-  const uploadDir = path.join(process.cwd(), "uploads");
-  console.log("Chemin du dossier uploads :", uploadDir);
-
-  fs.readdir(uploadDir, (err, files) => {
-    if (err) {
-      console.error("Erreur lors de la lecture du dossier :", err);
-      return res
-        .status(500)
-        .json({ error: "Erreur lors de la lecture du dossier" });
-    }
-
-    let totalSize = 0;
-    files.forEach((file) => {
-      const filePath = path.join(uploadDir, file);
-      totalSize += fs.statSync(filePath).size;
-    });
-
-    const sizeInGB = totalSize / (1024 * 1024 * 1024);
-    res.json({ sizeInGB });
-  });
-});
+router.get("/size", getUploadsSize);
 
 export default router;
